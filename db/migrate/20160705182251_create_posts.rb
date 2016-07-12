e02134e7d6fec9c20c352c3f105db573139ac0af
Alice Fonfa <alicefonfa@gmail.com>
2016-07-11 22:31:55 -0400

    added carrierwave

--- db/migrate/20160705182251_create_posts.rb
+++ db/migrate/20160705182251_create_posts.rb
@@ -2,7 +2,9 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.text :text
+      t.string :image
       t.timestamps null: false
+
     end
   end
 end
